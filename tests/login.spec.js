2e479e23cd536cb4580b431ef7c815e374678891
Jefferson <jefferson@educationjourney.com>
2024-11-13 21:46:11 -0300

    create sql injection test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -29,3 +29,18 @@ test('usuário inválido', async ({ page }) => {
 
   await expect(page.locator('#swal2-title')).toBeVisible();
 });
+
+// Este teste espera que o login falhe com entradas maliciosas.
+test('sql injection', async ({ page }) => {
+  await page.goto('https://automationpratice.com.br/login');
+  
+  const loginButtonSelector = '#btnLogin';
+  await page.isVisible(loginButtonSelector);
+  
+  await page.fill('#user', "' OR 1=1");
+  await page.fill('#password', "'");
+
+  await page.click(loginButtonSelector);
+
+  await expect(page.locator('#swal2-title')).toBeVisible();
+});
